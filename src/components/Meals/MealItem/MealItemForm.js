34c455a2717801f3cb5f5d50f239a874fad67839
l0mens <lomens@hotmail.fr>
2022-05-23 11:26:19 +0200

    EXPERIMENT-T-2 : Input fixed #close

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,13 +1,23 @@
-import classes from './MealItemForm.module.css'
-import Input from '../../Layout/Input'
+import classes from "./MealItemForm.module.css";
+import Input from "../../Layout/Input";
 
 const MealItemForm = (props) => {
-    return (
-        <form className={classes.form}>
-            <Input label="Amount" input={{id: 'amoun', type:"number", min : '1', max : '5', step : '1', defaultValue: '1'}}/>
-            <button type="button">+ Add</button>
-        </form>
-    )
-}
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: 'amount_' + props.id, // this changed!
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button type="button">+ Add</button>
+    </form>
+  );
+};
 
-export default MealItemForm
+export default MealItemForm;
